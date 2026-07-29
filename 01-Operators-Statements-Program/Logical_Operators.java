d705d566109b560115067eb7bb413da77c4b0d76
Jaswinder Singh <jaswinderree001@gmail.com>
2026-07-29 22:23:41 +0530

    Add files via upload

---
+++ 01-Operators-Statements-Program/Logical_Operators.java
@@ -0,0 +1,11 @@
+class Logical_Operators{
+    public static void main(String[] args) {
+        int a=10,b=5;
+        
+         // &&,||,! Logical Operators
+
+        System.out.println((a>b&&a<b));
+        System.out.println((a>b||a<b));
+        System.out.print(!(a>5));
+    }
+}
